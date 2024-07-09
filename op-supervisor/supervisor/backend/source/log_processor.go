7516e2e758f483e630d9596de6576c45913580f4
Adrian Sutton <adrian@oplabs.co>
2024-07-09 13:45:54 +1000

    Rebase: op-supervisor: Include executing message info when storing logs.
    Takes from aj/parse-exec-msg and makes the following updates:
    - uses upstream ABI definitions for identifier hash
    - removes the core recording functionality for the moment
    - fixes up inconsistent typing and merge conflicts due to rearranged packages

--- op-supervisor/supervisor/backend/source/log_processor.go
+++ op-supervisor/supervisor/backend/source/log_processor.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 
 	"github.com/ethereum-optimism/optimism/op-service/eth"
-	"github.com/ethereum-optimism/optimism/op-supervisor/supervisor/backend/types"
+	"github.com/ethereum-optimism/optimism/op-supervisor/supervisor/backend/source/contracts"
+	backendTypes "github.com/ethereum-optimism/optimism/op-supervisor/supervisor/backend/types"
 	supTypes "github.com/ethereum-optimism/optimism/op-supervisor/supervisor/types"
 	"github.com/ethereum/go-ethereum/common"
 	ethTypes "github.com/ethereum/go-ethereum/core/types"
@@ -13,22 +14,33 @@ import (
 )
 
 type LogStorage interface {
-	AddLog(chain supTypes.ChainID, logHash types.TruncatedHash, block eth.BlockID, timestamp uint64, logIdx uint32, execMsg *types.ExecutingMessage) error
+	AddLog(chain supTypes.ChainID, logHash backendTypes.TruncatedHash, block eth.BlockID, timestamp uint64, logIdx uint32, execMsg *backendTypes.ExecutingMessage) error
+}
+
+type EventDecoder interface {
+	DecodeExecutingMessageLog(log *ethTypes.Log) (backendTypes.ExecutingMessage, error)
 }
 
 type logProcessor struct {
-	chain    supTypes.ChainID
-	logStore LogStorage
+	chain        supTypes.ChainID
+	logStore     LogStorage
+	eventDecoder EventDecoder
 }
 
 func newLogProcessor(chain supTypes.ChainID, logStore LogStorage) *logProcessor {
-	return &logProcessor{chain: chain, logStore: logStore}
+	return &logProcessor{
+		chain:        chain,
+		logStore:     logStore,
+		eventDecoder: contracts.NewCrossL2Inbox(),
+	}
 }
 
 func (p *logProcessor) ProcessLogs(_ context.Context, block eth.L1BlockRef, rcpts ethTypes.Receipts) error {
 	for _, rcpt := range rcpts {
 		for _, l := range rcpt.Logs {
 			logHash := logToHash(l)
+			// TODO: on rebase, I removed the ExecutingMessage decoding here.
+			// it will need to be rewritten to use the hash format instead, as applicable
 			err := p.logStore.AddLog(p.chain, logHash, block.ID(), block.Time, uint32(l.Index), nil)
 			if err != nil {
 				return fmt.Errorf("failed to add log %d from block %v: %w", l.Index, block.ID(), err)
@@ -38,12 +50,12 @@ func (p *logProcessor) ProcessLogs(_ context.Context, block eth.L1BlockRef, rcpt
 	return nil
 }
 
-func logToHash(l *ethTypes.Log) types.TruncatedHash {
+func logToHash(l *ethTypes.Log) backendTypes.TruncatedHash {
 	payloadHash := crypto.Keccak256(logToPayload(l))
 	msg := make([]byte, 0, 2*common.HashLength)
 	msg = append(msg, l.Address.Bytes()...)
 	msg = append(msg, payloadHash...)
-	return types.TruncateHash(crypto.Keccak256Hash(msg))
+	return backendTypes.TruncateHash(crypto.Keccak256Hash(msg))
 }
 
 func logToPayload(l *ethTypes.Log) []byte {
